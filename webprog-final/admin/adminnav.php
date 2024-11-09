39bc132b68dec3cf21d0bce9d21b241361c2a04e
tyluts <cyrusballon2@gmail.com>
2024-11-09 22:24:50 +0800

    Update adminnav.php

--- webprog-final/admin/adminnav.php
+++ webprog-final/admin/adminnav.php
@@ -9,9 +9,9 @@
             <nav class="nav ">
                 <div> 
                     <div class="nav_list my-auto"> 
-                        <a href="#" class="nav_link ">  <span class="nav_name fs-6">Dashboard</span> </a> 
-                    <a href="#" class="nav_link">  <span class="nav_name">Content </span> </a> 
-                    <a href="#" class="nav_link"> </i> <span class="nav_name">Account </span> </a> 
+                        <a href="#" class="nav_link ">  <span class="nav_name fs-6">Lorem</span> </a> 
+                    <a href="#" class="nav_link">  <span class="nav_name">Lorem </span> </a> 
+                    <a href="#" class="nav_link"> </i> <span class="nav_name">Lorem </span> </a> 
                 </div>
                 </div> <a href="#" class="nav_link"></i>  <span class="nav_name">Sign Out </span></a>
             </nav>
